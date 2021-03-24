c2288ec8e6c5cc947e2574336c4dfbfe1589c002
NatalyaLuzyanina <lnp3395@gmail.com>
2021-03-24 22:24:57 +0300

    Finished version

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,25 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IB Outlets
     @IBOutlet var redLabel: UIView!
     @IBOutlet var orangeLabel: UIView!
     @IBOutlet var greenLabel: UIView!
     
     @IBOutlet var startBottom: UIButton!
     
+    //MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
         startBottom.layer.cornerRadius = 10
-        // Do any additional setup after loading the view.
     }
 
     override func viewWillLayoutSubviews() {
        redLabel.layer.cornerRadius = redLabel.frame.width / 2
-       orangeLabel.layer.cornerRadius = orangeLabel.frame.size.width / 2
+       orangeLabel.layer.cornerRadius = orangeLabel.frame.width / 2
        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
    }
     
+    //MARK: - IB Actions
     @IBAction func start() {
         
         if redLabel.alpha != 1 && orangeLabel.alpha != 1 && greenLabel.alpha != 1 {
