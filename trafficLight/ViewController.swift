ac0f5e0fd03b530cbc080fa9484c045fe8b623b8
NatalyaLuzyanina <lnp3395@gmail.com>
2021-03-24 09:56:13 +0300

    Interface

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLabel: UIView!
+    @IBOutlet var orangeLabel: UIView!
+    @IBOutlet var greenLabel: UIView!
+    
+    @IBOutlet var startBottom: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        redLabel.layer.masksToBounds = true
+        redLabel.layer.cornerRadius =
+            redLabel.bounds.size.width / 2
+        
+        
+        
+        orangeLabel.layer.cornerRadius = orangeLabel.frame.size.width / 2
+        orangeLabel.clipsToBounds = true
+        
+        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
+        
+        
+        startBottom.layer.cornerRadius = 10
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func start() {
+    }
+    
 }
 
