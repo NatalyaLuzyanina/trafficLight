6f4f0b37519596557b4b8b4a4eb14c6a6b7914f7
NatalyaLuzyanina <lnp3395@gmail.com>
2021-03-24 12:06:27 +0300

    View Controller wrote

--- trafficLight/ViewController.swift
+++ trafficLight/ViewController.swift
@@ -18,25 +18,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLabel.layer.masksToBounds = true
-        redLabel.layer.cornerRadius =
-            redLabel.bounds.size.width / 2
-        
-        
-        
-        orangeLabel.layer.cornerRadius = orangeLabel.frame.size.width / 2
-        orangeLabel.clipsToBounds = true
-        
-        greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
-        
-        
         startBottom.layer.cornerRadius = 10
-        
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func start() {
-    }
+    override func viewWillLayoutSubviews() {
+       redLabel.layer.cornerRadius = redLabel.frame.width / 2
+       orangeLabel.layer.cornerRadius = orangeLabel.frame.size.width / 2
+       greenLabel.layer.cornerRadius = greenLabel.frame.width / 2
+   }
     
+    @IBAction func start() {
+        
+        if redLabel.alpha != 1 && orangeLabel.alpha != 1 && greenLabel.alpha != 1 {
+            redLabel.alpha = 1
+            startBottom.setTitle("NEXT", for: .normal)
+        } else if redLabel.alpha == 1 {
+            redLabel.alpha = 0.3
+            orangeLabel.alpha = 1
+        } else if orangeLabel.alpha == 1 {
+            orangeLabel.alpha = 0.3
+            greenLabel.alpha = 1
+        } else if greenLabel.alpha == 1 {
+            greenLabel.alpha = 0.3
+            redLabel.alpha = 1
+        }
+        
 }
-
+}
